cb49e35a2d6c338890ed9a1815915ab054c27cdc
Marcin Czajkowski <czajkowski81@gmail.com>
2026-09-03 15:47:07 +0200

    Point site at custom domain swissdataevents.ch

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://czaya2000.github.io',
-	base: '/swiss-it-ai-events',
+	site: 'https://swissdataevents.ch',
 });
